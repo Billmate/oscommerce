a8abbd70d40aca1f43e7006c48d4c21828c0b46a
jesper@boxedlogistics.se <jesper@boxedlogistics.se>
2015-06-10 10:22:30 +0200

    Some clean up with files that isnt in use anymore. And removed The Catalog folder.
    Fixes #26 and Fixes #23

---
+++ includes/classes/billmate/commonfunctions.php
@@ -0,0 +1,34 @@
+<?php
+if(!function_exists('getCountryID')){
+
+	function error_report($ip=''){
+		if( empty($ip)) return;
+
+		if( $_SERVER['REMOTE_ADDR'] == $ip ){
+			ini_set('display_errors', 1 );
+			error_reporting(E_ALL);
+		}
+	}
+	function mdie($ip='', $msg){
+		if( empty($ip)) return;
+
+		if( $_SERVER['REMOTE_ADDR'] == $ip ){
+			die($msg);
+		}
+	}
+	define('BILLPLUGIN_VERSION','2.0');
+	define('BILLMATE_VERSION',  "PHP:OsCommerce:".BILLPLUGIN_VERSION );
+
+	function getCountryID(){
+		return 209;
+		$country = strtoupper(shopp_setting('base_operations'));
+		switch($country){
+			case 'SE': return 209;
+			case 'FI': return 73;
+			case 'DK': return 59;
+			case 'NO': return 164;
+			default :
+				return 209;
+		}
+	}
+}
