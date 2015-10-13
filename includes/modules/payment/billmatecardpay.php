6cda4feb9ee373b55a58eb055f886a7e689f9315
Boxedsolutions <jesper@boxedlogistics.se>
2015-10-13 15:41:39 +0200

    Prepare release, Fixed products on order mail for card and bank

--- includes/modules/payment/billmatecardpay.php
+++ includes/modules/payment/billmatecardpay.php
@@ -829,6 +829,79 @@ class billmatecardpay {
 			$already_completed = false;
 			tep_session_register('already_completed');
 		}
+        $products_ordered = '';
+        $subtotal = 0;
+        $total_tax = 0;
+        for ($i=0, $n=sizeof($order->products); $i<$n; $i++) {
+// Stock Update - Joao Correia
+            if (STOCK_LIMITED == 'true') {
+                if (DOWNLOAD_ENABLED == 'true') {
+                    $stock_query_raw = "SELECT products_quantity, pad.products_attributes_filename
+                                FROM " . TABLE_PRODUCTS . " p
+                                LEFT JOIN " . TABLE_PRODUCTS_ATTRIBUTES . " pa
+                                ON p.products_id=pa.products_id
+                                LEFT JOIN " . TABLE_PRODUCTS_ATTRIBUTES_DOWNLOAD . " pad
+                                ON pa.products_attributes_id=pad.products_attributes_id
+                                WHERE p.products_id = '" . tep_get_prid($order->products[$i]['id']) . "'";
+// Will work with only one option for downloadable products
+// otherwise, we have to build the query dynamically with a loop
+                    $products_attributes = $order->products[$i]['attributes'];
+                    if (is_array($products_attributes)) {
+                        $stock_query_raw .= " AND pa.options_id = '" . $products_attributes[0]['option_id'] . "' AND pa.options_values_id = '" . $products_attributes[0]['value_id'] . "'";
+                    }
+                    $stock_query = tep_db_query($stock_query_raw);
+                } else {
+                    $stock_query = tep_db_query("select products_quantity from " . TABLE_PRODUCTS . " where products_id = '" . tep_get_prid($order->products[$i]['id']) . "'");
+                }
+                if (tep_db_num_rows($stock_query) > 0) {
+                    $stock_values = tep_db_fetch_array($stock_query);
+// do not decrement quantities if products_attributes_filename exists
+                    if ((DOWNLOAD_ENABLED != 'true') || (!$stock_values['products_attributes_filename'])) {
+                        $stock_left = $stock_values['products_quantity'] - $order->products[$i]['qty'];
+                    } else {
+                        $stock_left = $stock_values['products_quantity'];
+                    }
+                    tep_db_query("update " . TABLE_PRODUCTS . " set products_quantity = '" . $stock_left . "' where products_id = '" . tep_get_prid($order->products[$i]['id']) . "'");
+                    if ( ($stock_left < 1) && (STOCK_ALLOW_CHECKOUT == 'false') ) {
+                        tep_db_query("update " . TABLE_PRODUCTS . " set products_status = '0' where products_id = '" . tep_get_prid($order->products[$i]['id']) . "'");
+                    }
+                }
+            }
+
+// Update products_ordered (for bestsellers list)
+            tep_db_query("update " . TABLE_PRODUCTS . " set products_ordered = products_ordered + " . sprintf('%d', $order->products[$i]['qty']) . " where products_id = '" . tep_get_prid($order->products[$i]['id']) . "'");
+
+//------insert customer choosen option to order--------
+            $attributes_exist = '0';
+            $products_ordered_attributes = '';
+            if (isset($order->products[$i]['attributes'])) {
+                $attributes_exist = '1';
+                for ($j=0, $n2=sizeof($order->products[$i]['attributes']); $j<$n2; $j++) {
+                    if (DOWNLOAD_ENABLED == 'true') {
+                        $attributes_query = "select popt.products_options_name, poval.products_options_values_name, pa.options_values_price, pa.price_prefix, pad.products_attributes_maxdays, pad.products_attributes_maxcount , pad.products_attributes_filename
+                                   from " . TABLE_PRODUCTS_OPTIONS . " popt, " . TABLE_PRODUCTS_OPTIONS_VALUES . " poval, " . TABLE_PRODUCTS_ATTRIBUTES . " pa
+                                   left join " . TABLE_PRODUCTS_ATTRIBUTES_DOWNLOAD . " pad
+                                   on pa.products_attributes_id=pad.products_attributes_id
+                                   where pa.products_id = '" . $order->products[$i]['id'] . "'
+                                   and pa.options_id = '" . $order->products[$i]['attributes'][$j]['option_id'] . "'
+                                   and pa.options_id = popt.products_options_id
+                                   and pa.options_values_id = '" . $order->products[$i]['attributes'][$j]['value_id'] . "'
+                                   and pa.options_values_id = poval.products_options_values_id
+                                   and popt.language_id = '" . $languages_id . "'
+                                   and poval.language_id = '" . $languages_id . "'";
+                        $attributes = tep_db_query($attributes_query);
+                    } else {
+                        $attributes = tep_db_query("select popt.products_options_name, poval.products_options_values_name, pa.options_values_price, pa.price_prefix from " . TABLE_PRODUCTS_OPTIONS . " popt, " . TABLE_PRODUCTS_OPTIONS_VALUES . " poval, " . TABLE_PRODUCTS_ATTRIBUTES . " pa where pa.products_id = '" . $order->products[$i]['id'] . "' and pa.options_id = '" . $order->products[$i]['attributes'][$j]['option_id'] . "' and pa.options_id = popt.products_options_id and pa.options_values_id = '" . $order->products[$i]['attributes'][$j]['value_id'] . "' and pa.options_values_id = poval.products_options_values_id and popt.language_id = '" . $languages_id . "' and poval.language_id = '" . $languages_id . "'");
+                    }
+                    $attributes_values = tep_db_fetch_array($attributes);
+
+                    $products_ordered_attributes .= "\n\t" . $attributes_values['products_options_name'] . ' ' . $attributes_values['products_options_values_name'];
+                }
+            }
+
+
+            $products_ordered .= $order->products[$i]['qty'] . ' x ' . $order->products[$i]['name'] . ' (' . $order->products[$i]['model'] . ') = ' . $currencies->display_price($order->products[$i]['final_price'], $order->products[$i]['tax'], $order->products[$i]['qty']) . $products_ordered_attributes . "\n";
+        }
 
 		$email_order = STORE_NAME . "\n" .
 					 EMAIL_SEPARATOR . "\n" .
